977832cbbe8567a983fc0427a952ff19e342ae1d
Onalenna Junior Makhura <ojmakhura@roguesystems.co.bw>
2024-10-24 07:58:59 +0200

    first commit

---
+++ angular/src/test/SpringApplicationTest.java
@@ -0,0 +1,13 @@
+package bw.co.roguesystems.imis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImisAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ core/ApplicationCoreTestApplication.java
@@ -0,0 +1,7 @@
+package bw.co.roguesystems.imis;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImisCoreTestApplication {
+    
+}
